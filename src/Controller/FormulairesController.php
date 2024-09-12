885ec39a4f62f7b254a8e37634bc74d7dd1de0e5
leslie <l.habimana@interface3.be>
2024-09-12 10:14:17 +0200

    form controller + view creation

--- src/Controller/FormulairesController.php
+++ src/Controller/FormulairesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\AeroportType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,4 +14,17 @@ class FormulairesController extends AbstractController
     {
         return $this->render('formulaires/index.html.twig');
     }
+
+    #[Route('/formulaires/aeroport/afficher', name: 'app_aeroport_afficher')]
+    public function aeroportAfficher(): Response
+    {
+        $form = $this->createForm(AeroportType::class);
+        //dd($form);
+        $vars = ['formulaireAeroport'=>$form];
+
+
+        return $this->render('formulaires/aeroport_afficher.html.twig',$vars
+        );
+    }
 }
+
